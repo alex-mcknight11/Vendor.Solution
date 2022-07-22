8b9b4b6ee4887424b4d79651fb3b87ae2801b2af
alex-mcknight11 <alex.mcknight11@gmail.com>
2022-07-22 14:10:41 -0700

    models for bread and pastry

---
+++ Vendor/Models/Bread.cs
@@ -0,0 +1,48 @@
+using System.Collections.Generic;
+
+namespace Bakery.Models
+{
+  public class Bread
+  {
+    public string BreadType{get;set;}
+
+    private static List<Bread> _instances = new List<Bread>{};
+
+    public Bread(string breadType)
+    {
+      BreadType = breadType;
+      _instances.Add(this);
+    }
+
+    public static int GetLoafPrice()
+    {
+      int breadCount = _instances.Count;
+      int breadPrice = 0;
+      List<int>breadList = new List<int>{};
+      for(int i = 0; i <= breadCount; i++)
+      {
+        breadList.Add(i);
+      }
+      foreach(int bread in loafList)
+      {
+        if(bread % 3 == 0)
+        {
+          breadPrice += 0;
+        }
+        else
+        {
+          breadPrice += 5;
+        }
+      }
+      return breadPrice;
+    }
+    public static List<Bread> GetAllBread()
+    {
+      return _instances;
+    }
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+  }
+}
